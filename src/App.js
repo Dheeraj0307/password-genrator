460cc7640e2c058b6fa8c1a246ec7083e03fc93d
Dheeraj0307 <dheeraj390939@gmail.com>
2025-05-20 16:28:24 +0530

    change scenry

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./App.css";
-// import PasswordGenrator from './components/PasswordGenrator';
+import PasswordGenrator from "./components/PasswordGenrator";
 // import AddTodo from './components/AddTodo'
 // import { Todos } from './components/Todos';
 import { DragableTodo } from "./components/DragableTodo";
-import { Query } from "./components/Query";
 
 function App() {
   return (
     <div className="main-container">
-      {/* <PasswordGenrator /> */}
+      <PasswordGenrator />
       {/* <div className='todo-container'>
         <AddTodo />
         <Todos />
       </div> */}
 
       {/* <DragableTodo /> */}
-
-      <Query />
     </div>
   );
 }
